4ed3e8fa0df7a2f541ede617ad16f0ca06cef78b
shreybajiya19 <161223262+shreybajiya19@users.noreply.github.com>
2024-07-06 13:11:30 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -18,7 +18,7 @@ def main():
     stock_symbol = st.text_input('Enter stock symbol (e.g., AAPL):').upper()
 
     # User input for number of days to predict
-    predict_days = st.number_input('Enter number of days to predict:', min_value=1)
+    predict_days = st.number_input('Enter number of days to predict:')
 
     # Predict button
     if st.button('Predict') and stock_symbol != '' and predict_days != 0:
